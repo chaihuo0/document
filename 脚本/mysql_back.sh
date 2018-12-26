de6588f6a19a9784f4af9927aac50b16538d720d
chaihuo <1292185523@qq.com>
2018-12-26 13:19:31 +0800

    Update mysql_back.sh

--- 脚本/mysql_back.sh
+++ 脚本/mysql_back.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mydate=`date -d "yesterday" +"%Y%m%d"`
-docker exec -it mysql_containers /usr/bin/mysqldump -uroot -p'0o0o0o!@#dayingjia'  --all-databases  --hex-blob  --force --flush-logs --routines --log-error=/mysql/err${mydate}.log > /mysql/db${mydate}.sql
+docker exec -it mysql_containers /usr/bin/mysqldump -uroot -p'1111'  --all-databases  --hex-blob  --force --flush-logs --routines --log-error=/mysql/err${mydate}.log > /mysql/db${mydate}.sql
 cd /tgwdb/backup/mysql/
 tar -zcf db${mydate}.tar.gz db${mydate}.sql
 rm -rf /tgwdb/backup/mysql/db${mydate}.sql
